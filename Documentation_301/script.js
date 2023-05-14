3b33150c72285d281bce8652e6e183d5708a999a
Aathithya Jegatheesan <87900917+Aathithya-J@users.noreply.github.com>
2023-05-14 21:03:13 +0800

    Delete Documentation_301 directory

--- Documentation_301/script.js
+++
@@ -1,8 +0,0 @@
-function toggleElements() {
-    const elementsDiv = document.getElementById("elements");
-    if (elementsDiv.style.display === "none") {
-        elementsDiv.style.display = "block";
-    } else {
-        elementsDiv.style.display = "none";
-    }
-}
