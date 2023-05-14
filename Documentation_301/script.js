1150b19f3ca09dec0e6d081c01d54bb2d9eb5b4f
Aathithya Jegatheesan <87900917+Aathithya-J@users.noreply.github.com>
2023-05-14 21:02:56 +0800

    Add files via upload

---
+++ Documentation_301/script.js
@@ -0,0 +1,8 @@
+function toggleElements() {
+    const elementsDiv = document.getElementById("elements");
+    if (elementsDiv.style.display === "none") {
+        elementsDiv.style.display = "block";
+    } else {
+        elementsDiv.style.display = "none";
+    }
+}
